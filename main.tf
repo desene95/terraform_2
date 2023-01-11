73ab62534af0aeaa2ef4538a8f772db6a6222df2
Damian Esene <damianesene@Damians-MacBook-Air.local>
2023-01-10 20:51:52 -0500

    Create Storage Account

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "this" {
-  name = "TestStorageAccoutDame"
+  name = "teststorageaccoutdame"
   resource_group_name      = azurerm_resource_group.rg.name
   location     = "Canada central"
   account_tier  = "Standard"
